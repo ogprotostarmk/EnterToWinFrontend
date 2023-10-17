9be8e7e2f372e7b43a35ab64ecc541aace69b18a
dominiclsa <108898825+dominiclsa@users.noreply.github.com>
2023-10-17 16:19:25 -0600

    working

--- src/pages/ramsForm.js
+++ src/pages/ramsForm.js
@@ -1,12 +1,23 @@
-import ramsHeaderImage from '../../public/rams/ramsHeaderImage.jpg'
+import ramsHeaderImage from '../../public/rams/ramsHeaderImage.png'
 import footerImage from '../../public/rams/footerImage.jpeg'
 import contest from '../../public/rams/contest.png'
 import Form from '../components/Form'
 import Image from "next/image";
+import Head from 'next/head';
 
 const ramsForm = () => {
   return (
-    <div className="bg-ramsBackground bg-no-repeat bg-center block bg-cover w-full">
+    <>
+      <Head>
+        <title>Rams | ENTER TO WIN PROMO - Play Fun Inc</title>
+        <link rel="icon" href="/favicon.png" />
+        {/* <meta name="description" content=""/> */}
+        <meta property="og:title" content="Rams | ENTER TO WIN PROMO - Play Fun Inc"/>
+        {/* <meta property="og:description" content="Login to your Run Good Girls account. Enjoy exclusive content, interact with our stars, and be part of our community."/> */}
+        <meta property="og:url" content="https://www.playfuninc.org/ramsForm"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+      </Head>
+      <div className="bg-ramsBackground bg-no-repeat bg-center block bg-cover w-full">
       <Image src={ramsHeaderImage} alt="header image" className='w-full'/>
       <div className='flex flex-col xl:p-[0] sm:p-[30px] p-[20px]  items-center'>
               <Image src={contest} alt="content image"/>
@@ -18,6 +29,8 @@ const ramsForm = () => {
       </div>
       <Image src={footerImage} alt="footer image" className='w-full' />
   </div>
+    </>
+    
   )
 }
 
