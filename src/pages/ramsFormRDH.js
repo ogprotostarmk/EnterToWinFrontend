739887a5d5241bb47752b5fb763d6431d2682e2f
dominiclsa <108898825+dominiclsa@users.noreply.github.com>
2023-10-18 17:55:07 -0600

    working

--- src/pages/ramsFormRDH.js
+++ src/pages/ramsFormRDH.js
@@ -20,7 +20,11 @@ const ramsFormRDH = () => {
       <div className="bg-ramsBackground bg-no-repeat bg-center block bg-cover w-full">
       <Image src={ramsHeaderImage} alt="header image" className='w-full'/>
       <div className='flex flex-col xl:p-[0] sm:p-[30px] p-[20px]  items-center'>
-              <Image src={contest} alt="content image"/>
+              <div className='flex flex-col self-center lg:my-[40px] sm:my-[30px] my-[20px]'>
+                <h1 className='text-white lg:text-[68px] sm:text-[34px] font-[600] self-center text-[24px] text-center'>CONTEST ENTRY FORM</h1>
+                <p className='text-white lg:text-[32px] sm:text-[20px] text-[14px] text-center'>Enter to win tickets to a Los Angeles Rams home game, prize packages, signed merchandise & much more.</p>
+              </div>
+              {/* <Image src={contest} alt="content image"/> */}
               <div className='max-w-[1024px] flex flex-col justify-center items-center'>
                 <p className='sm:text-4xl text-3xl mb-[8px] text-white font-[500]'>Fill out below:</p>
                 <p className='sm:text-[18px] text-[16px] text-white font-[300] mb-[30px]'>Fields marked with <span className="text-[red] font-bold text-[20px]"> *</span> are requiered</p>
